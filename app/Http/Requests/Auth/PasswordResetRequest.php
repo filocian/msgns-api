71df470d44068943cb2f89fdba4bd593196761f2
rodrigo.obalat <rodrigo.obalat@gmail.com>
2024-07-12 12:16:42 +0200

    adjusted reset password request validation rule

--- app/Http/Requests/Auth/PasswordResetRequest.php
+++ app/Http/Requests/Auth/PasswordResetRequest.php
@@ -5,14 +5,21 @@ declare(strict_types=1);
 namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Validator;
 
 final class PasswordResetRequest extends FormRequest
 {
+	protected function prepareForValidation()
+	{
+		Validator::extend('custom_password', function($attribute, $value, $parameters, $validator) {
+			return preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?:{}|_<=>-]).*$/', $value);
+		}, 'Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character.');
+	}
 	public function rules(): array
 	{
 		return [
 			'token' => 'required|string',
-			'password' => 'required|string',
+			'password' => 'required|string:min:6|custom_password',
 		];
 	}
 }

262c71ffb64006e983b6e659b263de75f0b644c0
rodrigo.obalat <rodrigo.obalat@gmail.com>
2025-04-04 17:23:41 +0200

    adjusted blade redirection method now using button

--- resources/views/whatsapp/whatsapp-redirection.blade.php
+++ resources/views/whatsapp/whatsapp-redirection.blade.php
@@ -2,22 +2,17 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<title>Redirigiendo a WhatsApp...</title>
+	<title>Ir a WhatsApp</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<p>Un momento... estamos redirigiéndote a WhatsApp.</p>
+<p>Toca el botón para abrir WhatsApp:</p>
+<button onclick="abrirWhatsapp()">Abrir WhatsApp</button>
 
 <script>
-	setTimeout(() => {
-		const newWindow = window.open("{{ $url }}", "_blank");
-
-		// Por si el navegador bloquea el popup
-		if (!newWindow) {
-			window.location.href = "{{ $url }}";
-		}
-
-		}, 1500); // 1.5 segundos de delay
+	function abrirWhatsapp() {
+		window.location.href = "{{ $url }}";
+	}
 </script>
 </body>
 </html>

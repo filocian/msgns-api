fd1127e53d7e4d193a602a6789d12c711fa9fdf1
messagenes <165073282+messagenes@users.noreply.github.com>
2026-03-28 12:30:42 +0100

    chore(openapi): isolate swagger docs artifacts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ namespace App\Providers;
 
 use App\Infrastructure\Repositories\DynamoDb\DynamoDbRepository;
 use Illuminate\Support\ServiceProvider;
+use L5Swagger\L5SwaggerServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,10 @@ final class AppServiceProvider extends ServiceProvider
 	 */
 	public function register(): void
 	{
+		if ($this->app->environment(['local', 'testing', 'staging']) || env('SWAGGER_ENABLE')) {
+			$this->app->register(L5SwaggerServiceProvider::class);
+		}
+
 		$this->app->singleton(DynamoDbRepository::class, function ($app) {
 			return new DynamoDbRepository();
 		});
